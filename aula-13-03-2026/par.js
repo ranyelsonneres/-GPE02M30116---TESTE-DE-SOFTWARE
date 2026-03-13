b664e44cd58bbd4c946f97e7111f17c4d9af481c
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2026-03-13 12:46:34 -0300

    Create par.js

---
+++ aula-13-03-2026/par.js
@@ -0,0 +1,8 @@
+//função para verificar se o número é par
+
+function ehPar(numero) {
+    return numero % 2 == 0;
+}
+
+//exportar a função para ser utilizada na ferramenta de teste
+module.exports = ehPar;
